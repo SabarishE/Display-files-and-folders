dabf4c95ca39d1d297a7960203eb8321b0bfb6d0
SabarishE <sabarishtrblzr@gmail.com>
2021-08-07 09:28:44 +0530

    test-commit

--- index.js
+++ index.js
@@ -21,36 +21,36 @@ console.log("[▪] >>> file");
 console.log("[▣ ] >>> folder");
 
 
-function c(tpath){
-    var display=[];
+// function c(tpath){
+//     var display=[];
 
-//--------fs. readdirSync() method is used to synchronously read the contents of a given directory
+// //--------fs. readdirSync() method is used to synchronously read the contents of a given directory
 
-var files =fs.readdirSync(tpath);
-for( var x in files){
+// var files =fs.readdirSync(tpath);
+// for( var x in files){
 
-    var next= path.join(tpath,files[x])
+//     var next= path.join(tpath,files[x])
 
-//--------fs.statSync() method is used to return details about the given file path
+// //--------fs.statSync() method is used to return details about the given file path
 
-    if(fs.statSync(next).isDirectory())
-    {
-        let base=path.basename(next);
+//     if(fs.statSync(next).isDirectory())
+//     {
+//         let base=path.basename(next);
    
-        let folder={};
-        folder[` [▣ ] ☛  ${base}`]=c(next);
-         display=[...display,JSON.stringify({folder:folder}).replace(/\\/g, '') ];
+//         let folder={};
+//         folder[` [▣ ] ☛  ${base}`]=c(next);
+//          display=[...display,JSON.stringify({folder:folder}).replace(/\\/g, '') ];
         
-    }
-    else
-    {
-        display.push(JSON.stringify({file:" [▪] ☛  "+path.basename(next)}).replace(/\\/g, ''))
-    }
-}
-return display;
-}
-// ------- the output is diplayed as array of nested objects
-console.log(c(testpath));
+//     }
+//     else
+//     {
+//         display.push(JSON.stringify({file:" [▪] ☛  "+path.basename(next)}).replace(/\\/g, ''))
+//     }
+// }
+// return display;
+// }
+// // ------- the output is diplayed as array of nested objects
+// console.log(c(testpath));
 
 let sampleArr=[
     '{"file":" [▪] ☛  4+1.PNG"}',
